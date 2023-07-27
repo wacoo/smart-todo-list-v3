702abaf36d924f0ecb77420f529445558b33e5d7
wac <wabaham9@gmail.com>
2023-07-27 16:32:21 +0300

    Check linter error

--- src/test/add_remove.test.js
+++ src/test/add_remove.test.js
@@ -1,5 +1,5 @@
-import task  from '../tasks.js';
-import Task from '../one_task.js'
+import task from '../tasks.js';
+import Task from '../one_task.js';
 
 const description = 'Eat break fast';
 const description2 = 'Go to work';
@@ -8,20 +8,20 @@ const tsk1 = new Task();
 const tsk2 = new Task();
 
 describe('Test add and remove tasks', () => {
-    test('test task.addTask()', () => {
-        tsk1.index = 1;
-        tsk1.description = description;
-        tsk1.completed = false;
+  test('test task.addTask()', () => {
+    tsk1.index = 1;
+    tsk1.description = description;
+    tsk1.completed = false;
 
-        task.addTask(tsk1);
-        expect(task.taskCollection[0].description).toBe(description);
-    });
-    test('test task.removeTask()', () => {
-        tsk2.index = 2;
-        tsk2.description = description2;
-        tsk2.completed = false;
-        task.addTask(tsk2);
-        task.removeTask(1);
-        expect(task.taskCollection.length).toBe(1);
-    });
+    task.addTask(tsk1);
+    expect(task.taskCollection[0].description).toBe(description);
+  });
+  test('test task.removeTask()', () => {
+    tsk2.index = 2;
+    tsk2.description = description2;
+    tsk2.completed = false;
+    task.addTask(tsk2);
+    task.removeTask(1);
+    expect(task.taskCollection.length).toBe(1);
+  });
 });
