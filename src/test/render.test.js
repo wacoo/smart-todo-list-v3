8fcc9f516711b6bd3e0d39f2870535b3d06bf323
wac <wababam9@gmail.com>
2023-07-27 05:29:51 -0700

    Add test files

--- src/test/render.test.js
+++ src/test/render.test.js
@@ -11,7 +11,7 @@ const description2 = 'Go to work';
 const status = true;
 
 describe('Test edit and remove tasks functions', () => {
-    /*test('test editTask()', () => {
+    test('test editTask()', () => {
         tsk1.index = 1;
         tsk1.description = description;
         tsk1.completed = false;
@@ -29,42 +29,6 @@ describe('Test edit and remove tasks functions', () => {
         task.addTask(tsk1);
         task.updateCompleted(status, 0);
         expect(task.taskCollection[0].completed).toBe(true);
-    });*/
-
-    test('test removeAllCompleted()', () => {
-        tsk1.index = 1;
-        tsk1.description = description;
-        tsk1.completed = false;
-
-        task.addTask(tsk1);
-
-        tsk2.index = 2;
-        tsk2.description = description;
-        tsk2.completed = false;
-
-        task.addTask(tsk2);
-
-        tsk3.index = 3;
-        tsk3.description = description;
-        tsk3.completed = true;
-
-        task.addTask(tsk3);
-
-        tsk4.index = 4;
-        tsk4.description = description;
-        tsk4.completed = true;
-
-        task.addTask(tsk4);
-
-        task.removeAllCompleted();
-        let completedCount = 0;
-
-        task.taskCollection.forEach((ts) => {
-            if(ts.completed === true) {
-                completedCount += 1;
-            }
-        })
-        expect(completedCount).toBe(1);
     });
 
 });
